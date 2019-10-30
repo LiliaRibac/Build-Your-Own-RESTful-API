9c248ae802185150c76dfa6c77223b2f554dace1
LiliaRibac <ribac.lilia@gmai.com>
2019-10-30 10:56:03 -0700

    first commit

---
+++ Wiki-API/app.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+//TODO
+
+mongoose.connect("mongodb://localhost:27017/Wiki-API", {
+  useUnifiedTopology: true
+})
+
+ const wikiSchema={
+   title:String,
+   content:String
+ };
+
+ const Item = mongoose.model("Item", wikiSchema);
+ 
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
